1721426bb61d604c39f02833530cb7f6097183c7
Taehoon Kim <carpedm20@gmail.com>
2015-11-12 03:21:04 -0600

    add merge_dumps for mahout

--- evaluation/merge_dumps.py
+++ evaluation/merge_dumps.py
@@ -2,12 +2,21 @@ from glob import glob
 
 from utils import *
 
+mode = 'mahout'
+
 lines = []
-for dump_file in glob('./dump*.data'):
-    with open(dump_file) as f:
-        lines.extend(f.readlines())
+if mode=='mahout':
+    for dump_file in glob('./dump*.data'):
+        with open(dump_file) as f:
+            lines.extend([l.replace(',1\n',',3\n').replace(',-1',',0').replace(',','\t') for l in f.readlines()])
 
-        header(" # of lines until %s : %s" % (dump_file, len(lines)))
+            header(" # of lines until %s : %s" % (dump_file, len(lines)))
+elif mode=='spark':
+    for dump_file in glob('./dump*.data'):
+        with open(dump_file) as f:
+            lines.extend(f.readlines())
 
-with open('total_dump.data', 'w') as f:
+            header(" # of lines until %s : %s" % (dump_file, len(lines)))
+
+with open('total_dump_%s.data' % mode, 'w') as f:
     f.writelines(lines)
